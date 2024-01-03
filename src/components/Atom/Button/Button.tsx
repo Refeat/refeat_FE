3be67fe9364855e202a3ef3a202bc8c4d7528dec
1nxeo <y.1nseooo@gmail.com>
2024-01-03 20:29:02 +0900

    feat: edit props #2

--- src/components/Atom/Button/Button.tsx
+++ src/components/Atom/Button/Button.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { ComponentProps } from "react";
 
-export default function Button() {
+export default function Button({
+  children,
+  onClick,
+}: ComponentProps<"button">) {
   return (
-    <button className="w-[320px] h-[50px] bg-black text-white">Button</button>
+    <button
+      className="w-[320px] h-[50px] bg-black text-white"
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
