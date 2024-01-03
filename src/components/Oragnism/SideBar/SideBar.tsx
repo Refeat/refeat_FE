b248ba33f6172b31a333265ddc43923afb063df4
1nxeo <y.1nseooo@gmail.com>
2024-01-03 20:28:51 +0900

    feat: add new project button in sidebar #9

--- src/components/Oragnism/SideBar/SideBar.tsx
+++ src/components/Oragnism/SideBar/SideBar.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Atom/Button/Button";
 import Flex from "@/components/Atom/Flex/Flex";
 import Logo from "@/components/Atom/Logo/Logo";
 import ProfileCard from "@/components/Molecule/ProfileCard/ProfileCard";
@@ -8,6 +9,7 @@ export default function SideBar() {
     <Flex dir="col" ai="center" jc="start" className="border w-1/5">
       <Logo size={20} className="my-[40px]" />
       <ProfileCard name="Sangmin Lee" />
+      <Button>+ New Project</Button>
     </Flex>
   );
 }
