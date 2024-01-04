c2f6ce3af8c2b8e93b94923f2cdf3fdd4a3374af
1nxeo <y.1nseooo@gmail.com>
2024-01-04 16:09:54 +0900

    fix: add button props for routing #2

--- src/components/Atom/Button/Button.tsx
+++ src/components/Atom/Button/Button.tsx
@@ -1,13 +1,23 @@
+"use client";
+
+import { useRouter } from "next/navigation";
 import React, { ComponentProps } from "react";
 
-export default function Button({
-  children,
-  onClick,
-}: ComponentProps<"button">) {
+interface ButtonProps extends ComponentProps<"button"> {
+  href?: string;
+}
+
+export default function Button({ children, onClick, href }: ButtonProps) {
+  const router = useRouter();
+
+  const onClickHandler = (e: React.MouseEvent<HTMLButtonElement>) => {
+    onClick && onClick(e);
+    href && router.push(href);
+  };
   return (
     <button
-      className="w-[320px] h-[50px] bg-black text-white"
-      onClick={onClick}
+      className="w-3/5 h-[40px] bg-black text-white"
+      onClick={onClickHandler}
     >
       {children}
     </button>
