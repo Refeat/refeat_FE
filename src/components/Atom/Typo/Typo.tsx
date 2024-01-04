271ebe7975e18e4c481d6352b498f71fe81e968f
1nxeo <y.1nseooo@gmail.com>
2024-01-04 16:09:27 +0900

    fix: edit tailwild config to apply typo setting #7

--- src/components/Atom/Typo/Typo.tsx
+++ src/components/Atom/Typo/Typo.tsx
@@ -1,9 +1,53 @@
 import React, { ComponentPropsWithoutRef } from "react";
 
 interface TypoProps extends ComponentPropsWithoutRef<"span"> {
-  type?: string;
+  type:
+    | "body9"
+    | "body8"
+    | "body7"
+    | "body6"
+    | "body5"
+    | "body4"
+    | "body3"
+    | "body2"
+    | "body1"
+    | "body0"
+    | "link4"
+    | "link3"
+    | "link2"
+    | "link1"
+    | "error1"
+    | "display3"
+    | "display2"
+    | "display1"
+    | "headline2"
+    | "headline1"
+    | "subhead4"
+    | "subhead3"
+    | "subhead2"
+    | "subhead1";
 }
 
 export default function Typo({ children, type, className }: TypoProps) {
-  return <span className={`${className}`}>{children}</span>;
+  const isTitle =
+    type === "display3" ||
+    type === "display2" ||
+    type === "display1" ||
+    type === "headline2" ||
+    type === "headline1" ||
+    type === "subhead4" ||
+    type === "subhead3" ||
+    type === "subhead2" ||
+    type === "subhead1";
+  const fontFamily = isTitle ? "font-title" : "font-typo";
+  const fontWeight = isTitle ? "font-bold" : "font-medium";
+  // const typoSize = sizes[type as keyof typeof sizes];
+  // const typoHeight = heights[type as keyof typeof heights];
+  // const isDecoration = type.includes("link") ? true : false;
+
+  return (
+    <span className={`${className} ${fontFamily} ${fontWeight}`}>
+      {children}
+    </span>
+  );
 }
